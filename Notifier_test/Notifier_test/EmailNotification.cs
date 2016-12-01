7c585f4bf63c1db8474396263cf8d322917945a7
testtoottest <testtoottest@gmail.com>
2016-12-01 22:49:31 +0100

    email notification

---
+++ Notifier_test/Notifier_test/EmailNotification.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Mail;
+using System.Net.Mime;
+using System.Net;
+using System.Collections;
+
+namespace EmailNotifier_1
+{
+    class Email
+    {
+
+        public static void sendMessage()
+        {
+
+            MailMessage message = new MailMessage();
+            message.To.Add(new MailAddress("testtoottest@gmail.com"));
+            message.To.Add(new MailAddress("testtoottest@gmail.com"));
+            message.From = new System.Net.Mail.MailAddress("testtoottest@gmail.com");
+            message.Subject = "test";
+            message.Body = "super email notifier";
+
+            SmtpClient client = new SmtpClient();
+            client.Port = 587;
+            client.EnableSsl = true;
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.UseDefaultCredentials = false;
+            client.Credentials = new System.Net.NetworkCredential("testtoottest@gmail.com", "test1234.");
+            client.Host = "smtp.gmail.com";
+
+            message.IsBodyHtml = true;
+            try
+            {
+                client.Send(message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception caught in CreateMessageWithAttachment(): {0}",
+                            ex.ToString());
+            }
+
+        }
+    }
+}
